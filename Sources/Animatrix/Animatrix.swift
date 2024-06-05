bf2fd7eea5d9445a5174283d9f8d85e7fec2f46a
Матвей Авдеев <130132470+MatveiSW@users.noreply.github.com>
2024-06-05 16:06:38 +0300

    fix bugs

--- Sources/Animatrix/Animatrix.swift
+++ Sources/Animatrix/Animatrix.swift
@@ -3,6 +3,8 @@
 
 import UIKit
 
+import UIKit
+
 public enum Animation {
     case appearance
     case disappearance
@@ -11,27 +13,26 @@ public enum Animation {
     case pulse
     case explode
     case complexAnimation
-    case advancedAnimation
+    case advancedAnimation // Новая анимация
 }
 
 public struct Animatrix {
     public static func startAnimationIn(view: UIView, animation: Animation, duration: Double? = nil) {
         switch animation {
-
         case .appearance:
             appearance(view: view, duration: duration ?? 2)
         case .disappearance:
             disappearance(view: view, duration: duration ?? 2)
         case .shakeHorizontal:
-            shakeHorizontal(view: view, duration: duration ?? 2)
+            shake(view: view, direction: "x", duration: duration ?? 0.6)
         case .shakeVertical:
-            shakeVertical(view: view, duration: duration ?? 2)
+            shake(view: view, direction: "y", duration: duration ?? 0.6)
         case .pulse:
             pulse(view: view)
         case .explode:
             explode(view: view)
         case .complexAnimation:
-            complexAnimation(view: view )
+            complexAnimation(view: view)
         case .advancedAnimation:
             advancedAnimation(view: view)
         }
@@ -52,75 +53,64 @@ private func disappearance(view: UIView, duration: Double) {
     }
 }
 
-private func shakeHorizontal(view: UIView, duration: Double) {
-      let animation = CAKeyframeAnimation(keyPath: "transform.translation.x")
-      animation.timingFunction = CAMediaTimingFunction(name: .linear)
-      animation.values = [-30, 30, -20, 20, -10, 10, -5, 5, -3, 3, 0]
-      animation.duration = 0.6
-      
-      view.layer.add(animation, forKey: "shake")
-  }
-
-private func shakeVertical(view: UIView, duration: Double) {
-      let animation = CAKeyframeAnimation(keyPath: "transform.translation.y")
-      animation.timingFunction = CAMediaTimingFunction(name: .linear)
-      animation.values = [-30, 30, -20, 20, -10, 10, -5, 5, -3, 3, 0]
-      animation.duration = 0.6
-      
-      view.layer.add(animation, forKey: "shake")
-  }
+private func shake(view: UIView, direction: String, duration: Double) {
+    let animation = CAKeyframeAnimation(keyPath: "transform.translation.\(direction)")
+    animation.timingFunction = CAMediaTimingFunction(name: .linear)
+    animation.values = [-30, 30, -20, 20, -10, 10, -5, 5, -3, 3, 0]
+    animation.duration = duration
+    view.layer.add(animation, forKey: "shake")
+}
 
 private func pulse(view: UIView) {
-        let pulseAnimation = CABasicAnimation(keyPath: "transform.scale")
-        pulseAnimation.duration = 1
-        pulseAnimation.fromValue = 1.0
-        pulseAnimation.toValue = 1.2
-        pulseAnimation.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
-        pulseAnimation.autoreverses = true
-        pulseAnimation.repeatCount = Float.infinity
-        
-        view.layer.add(pulseAnimation, forKey: "pulse")
- }
+    let pulseAnimation = CABasicAnimation(keyPath: "transform.scale")
+    pulseAnimation.duration = 1
+    pulseAnimation.fromValue = 1.0
+    pulseAnimation.toValue = 1.2
+    pulseAnimation.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
+    pulseAnimation.autoreverses = true
+    pulseAnimation.repeatCount = Float.infinity
+    view.layer.add(pulseAnimation, forKey: "pulse")
+}
 
 private func explode(view: UIView) {
-       UIView.animate(withDuration: 0.2, animations: {
-           view.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
-           view.alpha = 0.0
-       }, completion: { _ in
-           view.transform = CGAffineTransform.identity
-           view.alpha = 1.0
-       })
- }
+    UIView.animate(withDuration: 0.2, animations: {
+        view.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+        view.alpha = 0.0
+    }, completion: { _ in
+        view.transform = CGAffineTransform.identity
+        view.alpha = 1.0
+    })
+}
 
 private func complexAnimation(view: UIView) {
-        // Создание анимации масштаба
-        let scaleAnimation = CABasicAnimation(keyPath: "transform.scale")
-        scaleAnimation.fromValue = 1.0
-        scaleAnimation.toValue = 1.5
-        scaleAnimation.duration = 1.0
-        scaleAnimation.autoreverses = true
-        scaleAnimation.repeatCount = Float.infinity
-        
-        // Создание анимации вращения
-        let rotationAnimation = CABasicAnimation(keyPath: "transform.rotation")
-        rotationAnimation.fromValue = 0
-        rotationAnimation.toValue = Double.pi * 2
-        rotationAnimation.duration = 2.0
-        rotationAnimation.repeatCount = Float.infinity
+    // Создание анимации масштаба
+    let scaleAnimation = CABasicAnimation(keyPath: "transform.scale")
+    scaleAnimation.fromValue = 1.0
+    scaleAnimation.toValue = 1.5
+    scaleAnimation.duration = 1.0
+    scaleAnimation.autoreverses = true
+    scaleAnimation.repeatCount = Float.infinity
+    
+    // Создание анимации вращения
+    let rotationAnimation = CABasicAnimation(keyPath: "transform.rotation")
+    rotationAnimation.fromValue = 0
+    rotationAnimation.toValue = Double.pi * 2
+    rotationAnimation.duration = 2.0
+    rotationAnimation.repeatCount = Float.infinity
 
-        // Создание анимации прозрачности
-        let opacityAnimation = CABasicAnimation(keyPath: "opacity")
-        opacityAnimation.fromValue = 1.0
-        opacityAnimation.toValue = 0.5
-        opacityAnimation.duration = 1.0
-        opacityAnimation.autoreverses = true
-        opacityAnimation.repeatCount = Float.infinity
-        
-        // Добавление всех анимаций к слою testView
-        view.layer.add(scaleAnimation, forKey: "scale")
-        view.layer.add(rotationAnimation, forKey: "rotation")
-        view.layer.add(opacityAnimation, forKey: "opacity")
-    }
+    // Создание анимации прозрачности
+    let opacityAnimation = CABasicAnimation(keyPath: "opacity")
+    opacityAnimation.fromValue = 1.0
+    opacityAnimation.toValue = 0.5
+    opacityAnimation.duration = 1.0
+    opacityAnimation.autoreverses = true
+    opacityAnimation.repeatCount = Float.infinity
+    
+    // Добавление всех анимаций к слою testView
+    view.layer.add(scaleAnimation, forKey: "scale")
+    view.layer.add(rotationAnimation, forKey: "rotation")
+    view.layer.add(opacityAnimation, forKey: "opacity")
+}
 
 private func advancedAnimation(view: UIView) {
     // Создание анимации масштаба
