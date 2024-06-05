4d0d17f9014149985167ceaba77878c883d2771f
Матвей Авдеев <130132470+MatveiSW@users.noreply.github.com>
2024-06-05 15:10:43 +0300

    add appearance animation

--- Sources/Animatrix/Animatrix.swift
+++ Sources/Animatrix/Animatrix.swift
@@ -1,22 +1,26 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-import Foundation
+import UIKit
 
 public enum Animation: String {
-    case slide
-    case print
+    case appearance
     
 }
 
 public struct Animatrix {
-    public static func thisPrint(text: Animation) {
-        switch text {
+    public static func thisPrint(view: UIView, animation: Animation) {
+        switch animation {
             
-        case .slide:
-            print("hello slide")
-        case .print:
-            print("hello print")
+        case .appearance:
+           appearance(view: view)
         }
+    }
+}
+
+private func appearance(view: UIView) {
+    view.alpha = 0
+    UIView.animate(withDuration: 2) {
+        view.alpha = 1
     }
 }
