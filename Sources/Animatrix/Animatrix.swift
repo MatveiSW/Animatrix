6b8114d8866e44213dd2c40649a67aa3112e819f
Матвей Авдеев <130132470+MatveiSW@users.noreply.github.com>
2024-06-05 13:23:17 +0300

    First Comit

--- Sources/Animatrix/Animatrix.swift
+++ Sources/Animatrix/Animatrix.swift
@@ -1,2 +1,10 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+
+public struct Animatrix {
+    static func helloWorld() {
+        print("HelloWorld")
+    }
+}
